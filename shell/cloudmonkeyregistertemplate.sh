5964ec91098ceadca120d829de148f3c65000f50
root <root@pc134.localdomain>
2015-10-29 15:39:59 +0800

    register

--- shell/cloudmonkeyregistertemplate.sh
+++ shell/cloudmonkeyregistertemplate.sh
@@ -9,8 +9,8 @@ hypervisor="KVM"
 url="http://192.168.122.1/template/manager-base7.qcow2"
 zonename="Zone1"
 cloudmonkey set display table
-zoneid=`cloudmonkey list zones filter=id,name name="Zone1"|grep "Zone1"|awk -F"[|]" '{print $3}'|sed -e "s/ //g"`
-ostypeid=`cloudmonkey list ostypes description="CentOS 6 (64-bit)"|grep "CentOS 6 (64-bit)"|awk -F"[|]" '{print $5}'|sed -e "s/ //g"`
+zoneid=`cloudmonkey list zones filter=id,name name="$zonename"|grep "$zonename"|awk -F"[|]" '{print $3}'|sed -e "s/ //g"`
+ostypeid=`cloudmonkey list ostypes description="$ostype"|grep "$ostype"|awk -F"[|]" '{print $5}'|sed -e "s/ //g"`
 cloudmonkey set display default
 cloudmonkey register template name="${name}" displaytext="${displaytext}" isfeatured=${featured} passwordenabled=${passwordenabled} ispublic=${public} ostypeid=${ostypeid} format=${format} hypervisor=${hypervisor} zoneid=${zoneid} url=${url}
 
