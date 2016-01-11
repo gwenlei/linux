a92e0921e7b295d9a27996554e30a9d49e2ab5ab
root <root@pc134.localdomain>
2016-01-11 16:09:22 +0800

    qga

--- ansible/qga/roles/qga/files/qgadownload2.ps1
+++ ansible/qga/roles/qga/files/qgadownload2.ps1
@@ -1,6 +1,2 @@
-cd C:\
-$src = 'http://192.168.0.82:9090/static/win/qemu-ga.zip'
-$des = "$env:win/qemu-ga.zip"
 Invoke-WebRequest -uri 'http://192.168.0.82:9090/static/win/qemu-ga.zip' -OutFile "C:\win\qemu-ga.zip"
-Unblock-File $des
 
