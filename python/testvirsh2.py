922a30998fad353c306a5d6afcbb177a07d2558a
root <root@pc134.localdomain>
2016-06-27 16:39:41 +0800

    ansible

--- python/testvirsh2.py
+++ python/testvirsh2.py
@@ -2,6 +2,8 @@
 import libvirt
 import os
 import sys
+import re
+import subprocess
 from collections import namedtuple
 
 from ansible.parsing.dataloader import DataLoader
@@ -24,23 +26,43 @@ fo = open("host", "wb")
 fo.write(obj['vnet0']['addrs'][0]['addr']+" ansible_ssh_user=root ansible_ssh_pass=engine")
 fo.close()
 
-variable_manager = VariableManager()
-loader = DataLoader()
+def simpletask():
+  variable_manager = VariableManager()
+  loader = DataLoader()
+  inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list='./host')
+  playbook_path = './main.yml'
+  if not os.path.exists(playbook_path):
+      print '[INFO] The playbook does not exist'
+      sys.exit()
+  Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
+  options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='root', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=None, check=False)
+  variable_manager.extra_vars = {'hosts': 'vmware'} # This can accomodate various other command line arguments.`
+  passwords = {}
+  pbex = PlaybookExecutor(playbooks=[playbook_path], inventory=inventory, variable_manager=variable_manager, loader=loader, options=options, passwords=passwords)
+  try:
+    results = pbex.run()
+  except BaseException:
+    pass  
+  return
 
-inventory = Inventory(loader=loader, variable_manager=variable_manager, host_list='./host')
-playbook_path = './main.yml'
+cmd='ansible-galaxy list'
+p=subprocess.Popen(args=cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,close_fds=True)
+(stdoutdata,stderrdata)=p.communicate()
+if p.returncode !=0:
+ print (cmd+'error')
 
-if not os.path.exists(playbook_path):
-    print '[INFO] The playbook does not exist'
-    sys.exit()
+i=0
+for r in str(stdoutdata).split("\n"):
+ j=0
+ for r2 in r.split(" "):
+   if j==0 : 
+     j+=1
+     continue
+   r22=re.sub(r',',"",r2)
+   foo = open("main.yml", "wb")
+   foo.write("- hosts: all\n  roles:\n  - "+r22)
+   foo.close()
+   print("playbook "+i+" "+r22)
+   simpletask()
+   break
 
-Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
-options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='root', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=None, check=False)
-
-variable_manager.extra_vars = {'hosts': 'vmware'} # This can accomodate various other command line arguments.`
-
-passwords = {}
-
-pbex = PlaybookExecutor(playbooks=[playbook_path], inventory=inventory, variable_manager=variable_manager, loader=loader, options=options, passwords=passwords)
-
-results = pbex.run()
